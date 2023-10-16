5d3cadb971a12ff5631971a485a2beb5c6f13b09
yuepixing <meet.jia@qq.com>
2023-10-16 20:31:54 +0800

    Add files via upload

---
+++ jupyter_lab_config.py
@@ -0,0 +1,7 @@
+c = get_config()
+c.ServerApp.ip = '0.0.0.0'
+c.ServerApp.port = 88
+c.ServerApp.root_dir = '/root'
+c.ServerApp.token = ''
+c.ServerApp.allow_root = True
+c.ExtensionApp.open_browser = False
